e9d0ddf42f79c1389ac28f6fe530f844c7ccdc92
HelloMandu <tjdals6695@gmail.com>
2020-11-15 00:50:45 +0900

    주차공간 css complete

--- src/containers/main/parking/ParkingEnrollContainer.js
+++ src/containers/main/parking/ParkingEnrollContainer.js
@@ -2,40 +2,39 @@ import React from 'react';
 /* Library */
 import useInput from '../../../hooks/useInput';
 
+import ParkingInfo from '../../../components/parking/ParkingInfo';
+import VerifyPhone from '../../../components/verifyphone/VerifyPhone';
+import CheckBox from '../../../components/checkbox/CheckBox';
 import Price from './Price';
 
-import ParkingInfo from '../../../components/parking/ParkingInfo';
-import ConfirmButton from '../../../components/button/ConfirmButton';
 import InputBox from '../../../components/inputbox/InputBox';
+import ConfirmButton from '../../../components/button/ConfirmButton';
 import ArrowSmall from '../../../static/asset/svg/ArrowSmall';
 
 import styles from './ParkingEnrollContainer.module.scss';
-import VerifyPhone from '../../../components/verifyPhone/VerifyPhone';
 
 const ParkingEnrollContainer = () => {
-
     const [point, handleChangePoint] = useInput(0);
     return (
         <div className={styles['parkingpayment-container']}>
             <ParkingInfo></ParkingInfo>
             <div className={styles['parkingpayment-wrapper']}>
-            <div className={styles['title']}>{'대여자 연락처'}</div>
+                <div className={styles['title']}>{'대여자 연락처'}</div>
                 <VerifyPhone></VerifyPhone>
             </div>
             <div className={styles['parkingpayment-wrapper']}>
                 <div className={styles['title']}>{'쿠폰 할인'}</div>
                 <div className={styles['verify-coupon']}>
-                    <select className={styles['coupon']} name="coupon">
-                        <option value="temp">
-                            오픈 이벤트 10% 할인 이벤트 쿠폰
-                        </option>
-                    </select>
+                    <div className={styles['coupon']} name="coupon">
+                        오픈 이벤트 10% 할인 이벤트 쿠폰
+                    </div>
                     <ArrowSmall rotate={180}></ArrowSmall>
                 </div>
             </div>
             <div className={styles['parkingpayment-wrapper']}>
                 <div className={styles['title']}>{'포인트 할인'}</div>
                 <InputBox
+                    className={'input-box'}
                     type={'text'}
                     value={point}
                     placeholder={'사용하실 포인트를 입력해주세요'}
@@ -54,15 +53,16 @@ const ParkingEnrollContainer = () => {
                 </div>
             </div>
             <div className={styles['parkingpayment-wrapper']}>
-                <div className={styles['title']}>{'결제수단'}</div>
+                <div className={styles['title']}>결제수단</div>
                 <div className={styles['verify-payment']}>
-                    <select className={styles['payment']} name="payment">
-                        <option value="temp">카카오페이</option>
-                    </select>
+                    <div className={styles['payment']} name="payment">
+                        카카오페이
+                    </div>
                     <ArrowSmall rotate={90}></ArrowSmall>
                 </div>
             </div>
             <Price></Price>
+            <CheckBox></CheckBox>
         </div>
     );
 };
