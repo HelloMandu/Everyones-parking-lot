b06c6eb6fc2b0f1513037a89cdd6239236a11f8e
JeongHyun <k557330@naver.com>
2020-11-14 17:14:09 +0900

    로그인 안내창, 이메일 로그인 퍼블리싱

--- src/containers/auth/SignInContainer.js
+++ src/containers/auth/SignInContainer.js
@@ -1,11 +1,52 @@
 import React from 'react';
+import { ButtonBase } from '@material-ui/core'
 /* Library */
 
-const SignInContainer = () => {
+import styles from './SignInContainer.module.scss';
+import logo from '../../static/asset/png/logo.png';
 
+import Naver from '../../static/asset/svg/naver'
+import Kakao from '../../static/asset/svg/kakao'
+import Facebook from '../../static/asset/svg/facebook'
+
+const SignInContainer = () => {
     return (
-        <div>
-            <h1>로그인 페이지</h1>
+        <div className={styles['container']}>
+            <div className={styles['logo']}>
+                <img src={logo} alt="" />
+            </div>
+
+            <input
+                type="email"
+                className={styles['input']}
+                name="email"
+                placeholder="이메일을 입력해주세요."
+            />
+            <input
+                type="password"
+                className={styles['input']}
+                name="password"
+                placeholder="비밀번호을 입력해주세요."
+            />
+
+            <div className={styles['right']}>
+                <ButtonBase className={styles['find']}>아이디</ButtonBase>
+                <span>|</span>
+                <ButtonBase className={styles['find']}>비밀번호</ButtonBase>
+            </div>
+
+            <div className={styles['button-wrapper']}>
+                <ButtonBase className={styles['button']} style={{color:"#FFFFFF", background:"#222222", fontWeight:"bold", fontSize:"16px"}} >로그인</ButtonBase>
+                <ButtonBase className={styles['button']} >회원가입</ButtonBase>
+            </div>
+
+            <div className={styles['social-text']}>소셜 간편 로그인</div>
+
+            <div className={styles['social-icon-wrapper']}>
+                <ButtonBase className={styles["social-icon"]} style={{background: "#00BF19"}} ><Naver /></ButtonBase>
+                <ButtonBase className={styles["social-icon"]} style={{background: "#FCE000"}} ><Kakao /></ButtonBase>
+                <ButtonBase className={styles["social-icon"]} style={{background: "#4267B2"}} ><Facebook /></ButtonBase>
+            </div>
         </div>
     );
 };
