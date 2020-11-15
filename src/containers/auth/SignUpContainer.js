689a75e208aa75aea87f76b3be58d3ccbcb38948
JeongHyun <k557330@naver.com>
2020-11-15 20:54:19 +0900

    회원가입 퍼블리싱 미완

--- src/containers/auth/SignUpContainer.js
+++ src/containers/auth/SignUpContainer.js
@@ -9,7 +9,71 @@ import {
     isCellPhoneForm,
 } from '../../lib/formatChecker';
 
+import CheckBox from '../../components/checkbox/CheckBox';
+
 import styles from './SignUpContainer.module.scss';
+import ArrowSmall from '../../static/asset/svg/ArrowSmall';
+
+const checkList = [
+    {
+        id: 1,
+        checked: false,
+        description: '이용약관 필수 동의',
+    },
+    {
+        id: 2,
+        checked: false,
+        description: '개인정보 처리방침 필수 동의',
+    },
+    {
+        id: 3,
+        checked: false,
+        description: '쿠폰 / 이벤트 알림 선택 동의',
+        subDescription:
+            'SMS, 이메일을 통해 파격할인/이벤트/쿠폰 정보를 받아보실 수 있습니다.',
+    },
+];
+
+const DATE = new Date(1970, 1, 1);
+const CURRENT = new Date();
+
+const YEAR = [];
+const MONTH = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12];
+const DAY = [
+    1,
+    2,
+    3,
+    4,
+    5,
+    6,
+    7,
+    8,
+    9,
+    10,
+    11,
+    12,
+    13,
+    14,
+    15,
+    16,
+    17,
+    18,
+    19,
+    20,
+    21,
+    22,
+    23,
+    24,
+    25,
+    26,
+    27,
+    28,
+    29,
+    30,
+    31,
+];
+
+for (let i = DATE.getFullYear(); i <= CURRENT.getFullYear(); i++) YEAR.push(i);
 
 const SignUpContainer = () => {
     const [email, onChangeEmail, isEmail] = useInput('', isEmailForm);
@@ -19,7 +83,6 @@ const SignUpContainer = () => {
         isPasswordForm,
     );
     const [passwordCheck, onChangePasswordCheck] = useInput('');
-    const [birth, onChangeBirth] = useInput('');
     const [phone, onChangePhone, isPhone] = useInput('', isCellPhoneForm);
 
     const onClickSignUp = () => {
@@ -82,16 +145,35 @@ const SignUpContainer = () => {
 
             <div className={styles['input-wrapper']}>
                 <div className={styles['input-title']}>생년월일</div>
-                <InputBox
-                    className={'input-bar'}
-                    type={'text'}
-                    value={birth}
-                    placeholder={'생년월일을 입력해주세요.'}
-                    onChange={onChangeBirth}
-                    onKeyDown={(e) => {
-                        if (e.key === 'Enter') onClickSignUp();
-                    }}
-                />
+
+                <div className={styles['select-wrapper']}>
+                    <div className={styles['select-item']}>
+                        <select className={styles['select']}>
+                            {YEAR.map((y) => (
+                                <option key={y}>{y}년</option>
+                            ))}
+                        </select>
+                        <ArrowSmall rotate={180} />
+                    </div>
+
+                    <div className={styles['select-item']}>
+                        <select className={styles['select']}>
+                            {MONTH.map((m) => (
+                                <option key={m}>{m}월</option>
+                            ))}
+                        </select>
+                        <ArrowSmall rotate={180} />
+                    </div>
+
+                    <div className={styles['select-item']}>
+                        <select className={styles['select']}>
+                            {DAY.map((d) => (
+                                <option key={d}>{d}일</option>
+                            ))}
+                        </select>
+                        <ArrowSmall rotate={180} />
+                    </div>
+                </div>
             </div>
 
             <div className={styles['input-wrapper']}>
@@ -105,6 +187,13 @@ const SignUpContainer = () => {
                     onKeyDown={(e) => {
                         if (e.key === 'Enter') onClickSignUp();
                     }}
+                />
+            </div>
+
+            <div className={styles['check-box-wrapper']}>
+                <CheckBox
+                    allCheckTitle={'모두 동의합니다.'}
+                    checkListProps={checkList}
                 />
             </div>
         </div>
