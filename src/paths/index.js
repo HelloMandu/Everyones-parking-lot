dea0de35aa554ec9ac15021c93d2f045067387d1
ckdwns9121 <ckdwns9121@naver.com>
2020-11-16 17:30:44 +0900

    # 헤더 렌더

--- src/paths/index.js
+++ src/paths/index.js
@@ -66,3 +66,71 @@ export const Paths = {
     },
     api: 'https://www.naver.com/'
 };
+
+export const HeaderTitle={
+    main: {
+        index: '/',
+        detail: '/detail',
+        payment: '/payment',
+        payment_complete:'/payment_complete',
+        use : {
+            index : '',
+            list : '이용 내역',
+            detail :'',
+            cancel :'',
+            extend :'',
+        },
+        review:{
+            index:'/review',
+            list :'내가 작성한 리뷰',
+            write: '리뷰 쓰기',
+            detail :'리뷰 상세보기',
+        },
+        mypage:{
+            index: '/내 정보 수정',
+            point: '',
+            update:{
+                index : '',
+                name:'이름 변경',
+                password :'비밀번호 변경',
+                hp: '연락처 변경',
+                enrollment: '차량정보 등록',
+                birthday: '생년월일 변경',
+                profile: '',
+            }
+        },
+        parking:{
+            index :'',
+            manage: '내 주차공간 관리',
+            enrollment: {
+                index : '주차공간 등록',
+                coupon : '적용 쿠폰 선택',
+                payment : '결제수단 선택'
+            },
+            preview :'/parking/preview',
+        },
+        notification : '알림함',
+        coupon : '쿠폰',
+        event:{
+            index:'',
+            list :'이벤트',
+            detail :'이벤트',
+        },
+        support: '/support',
+    },
+    auth: {
+        index: '',
+        login :'',
+        signin: '로그인',
+        signup:'회원가입',
+        enrollment :'차량번호 등록',
+        sign_complete :'회원가입 완료',
+        find:{
+            index :'아이디/비밀번호 찾기',
+            email:'아이디 찾기',
+            password:'비밀번호 찾기',
+            email_complete:'',
+            password_complete:'비밀번호 재설정',
+        }
+    },
+}
