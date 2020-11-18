7f7eaea07f11ad44279e802be850d24dba81f773
박창준 <ckdwns9121@naver.com>
2020-11-18 23:21:31 +0900

    # 디테일 뷰작업

---
+++ src/components/nav/CustomTabs.js
@@ -0,0 +1,32 @@
+import React from 'react';
+import Tabs from '@material-ui/core/Tabs';
+import Tab from '@material-ui/core/Tab';
+
+import styles from './CustomTabs.module.scss';
+
+
+const CustomTabs = ({ idx, categories, onChange }) => {
+
+    const tabList = categories.map((category) => (
+        <Tab
+            label={category.ca_name}
+            key={category.ca_name}
+            className={styles['tab-item']}
+        />
+    ));
+
+    return (
+        <Tabs
+            value={idx}
+            onChange={onChange}
+            variant="scrollable"
+            scrollButtons="auto"
+            className={styles['tabs']}
+        >
+            {tabList}
+        </Tabs>
+
+    );
+};
+
+export default CustomTabs;
