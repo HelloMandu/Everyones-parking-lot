9df2a5595abe9bdddabecebbe295c36ac17f2f5e
JeongHyun <k557330@naver.com>
2020-11-19 12:56:17 +0900

    가입 완료 퍼블리싱

--- src/containers/auth/SignCompleteContainer.js
+++ src/containers/auth/SignCompleteContainer.js
@@ -1,15 +1,33 @@
 import React from 'react';
-import classNames from 'classnames/bind'
+import classNames from 'classnames/bind';
 
-import styles from './SignCompleteContainer.module.scss'
+import FixedButton from '../../components/button/FixedButton'
 
-const cx = classNames.bind(styles)
+import styles from './SignCompleteContainer.module.scss';
 
-const SignCompleteContainer =()=>{
-    return(
-        <div className={cx("container")}>
-            asd
+import Firework from '../../static/asset/svg/firework';
+
+const cx = classNames.bind(styles);
+
+const SignCompleteContainer = () => {
+    return (
+        <>
+        <div className={cx('container')}>
+            <div className={cx('area')}>
+                <div className={cx('icon-area')}>
+                    <Firework />
+                </div>
+
+                <div className={cx('ment')}>
+                    <div className={cx('title')}>가입을 축하합니다.</div>
+                    <div className={cx('content')}>
+                        홍길동님 회원가입이 완료되었습니다.
+                    </div>
+                </div>
+            </div>
         </div>
-    )
-}
+        <FixedButton button_name={"이용하러 가기"} disable={false} />
+        </>
+    );
+};
 export default SignCompleteContainer;
