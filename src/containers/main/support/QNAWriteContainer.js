89940aa26809919061d1107fbea6fb7431a59586
JeJn <jejn@JeJnui-MacBookAir.local>
2020-11-26 20:03:00 +0900

    #QNAWriteContainer

--- src/containers/main/support/QNAWriteContainer.js
+++ src/containers/main/support/QNAWriteContainer.js
@@ -1,4 +1,5 @@
-import React, { useRef } from 'react';
+import React, { useState, useRef, useCallback, useEffect } from 'react';
+import { ButtonBase, IconButton } from '@material-ui/core';
 /* Library */
 
 import FixedButton from '../../../components/button/FixedButton';
@@ -8,8 +9,96 @@ import InputBox from '../../../components/inputbox/InputBox';
 import useInput from '../../../hooks/useInput';
 /* Hooks */
 
+import Delete from '../../../static/asset/svg/parking/Delete';
+/* Static */
+
 import styles from './QNAWriteContainer.module.scss';
+import Plus from './Plus';
 /* StyleSheets */
+
+const FileItem = ({ file, onDelete }) => {
+    const [imgFile, setImgFile] = useState(null);
+    useEffect(() => {
+        const reader = new FileReader();
+        reader.onloadend = () => {
+            const base64 = reader.result;
+            if (base64) {
+                setImgFile(base64.toString());
+            }
+        };
+        if (file) {
+            reader.readAsDataURL(file);
+        }
+    }, [file]);
+    return (
+        <>
+            {imgFile && (
+                <div className={styles['file-item']}>
+                    <img
+                        className={styles['file-image']}
+                        src={imgFile}
+                        alt="file"
+                    />
+                    <IconButton
+                        className={styles['file-delete']}
+                        onClick={onDelete}
+                    >
+                        <Delete />
+                    </IconButton>
+                </div>
+            )}
+        </>
+    );
+};
+
+const FilesPicture = () => {
+
+    const [fileList, setFileList] = useState([]); //파일
+
+    const onChangeFileList = useCallback((e) => {
+        const { files } = e.target;
+        const newFileList = [];
+        for (let i = 0; i < files.length; i++) {
+            newFileList.push({ id: i + 1, file: files[i] });
+        }
+        setFileList(newFileList);
+    }, []);
+
+    const handleDeleteFile = useCallback(
+        (id) => setFileList(fileList.filter((file) => file.id !== id)),
+        [fileList],
+    );
+    // useEffect(() => {
+    //     setCheck(fileList.length >= 2);
+    // }, [setCheck, fileList]);
+
+    return (
+        <ul className={styles['file-list']}>
+            <ButtonBase className={styles['button']}>
+                <label htmlFor="file-setter">
+                    <Plus />
+                </label>
+            </ButtonBase>
+            <input
+                id="file-setter"
+                className={styles['input-files']}
+                onChange={onChangeFileList}
+                multiple="multiple"
+                type="file"
+                accept="image/gif, image/jpeg, image/png, image/svg"
+                formEncType="multipart/form-data"
+            />
+            {fileList.map(({ id, file }) => (
+                <li key={id}>
+                    <FileItem
+                        file={file}
+                        onDelete={() => handleDeleteFile(id)}
+                    ></FileItem>
+                </li>
+            ))}
+        </ul>
+    );
+};
 
 const QNAWriteContainer = () => {
 
@@ -67,6 +156,7 @@ const QNAWriteContainer = () => {
                     </div>
                     <div className={styles['files-wrap']}>
                         <div className={styles['text']}>첨부파일</div>
+                        <FilesPicture />
                     </div>
                 </div>
             </div>
