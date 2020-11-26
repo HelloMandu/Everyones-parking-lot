0a83ac8395f3ca68a3e15f08f24d728d7d831e84
JeJn <jejn@JeJnui-MacBookAir.local>
2020-11-26 16:25:40 +0900

    #QNAWrite -ing

--- src/containers/main/support/QNAWriteContainer.js
+++ src/containers/main/support/QNAWriteContainer.js
@@ -1,13 +1,78 @@
-import React from 'react';
-import { useLocation } from 'react-router-dom';
+import React, { useRef } from 'react';
 /* Library */
 
+import FixedButton from '../../../components/button/FixedButton';
+import InputBox from '../../../components/inputbox/InputBox';
+/* Components */
+
+import useInput from '../../../hooks/useInput';
+/* Hooks */
+
+import styles from './QNAWriteContainer.module.scss';
+/* StyleSheets */
+
 const QNAWriteContainer = () => {
-    const location = useLocation();
+
+    const subjectRef = useRef();
+    const questionRef = useRef();
+
+    const [email, onChangeEmail] = useInput('');
+    const [subject, onChangeSubject] = useInput('');
+    const [question, onChangeQuestion] = useInput('');
+
+    const onClickButton = () => {
+        // 업데이트 요청
+        alert('문의 작성')
+    }
+
     return (
-        <div>
-            1:1 문의 쓰기
-        </div>
+        <>
+            <div className={styles['container']}>
+                <div className={styles['data-area']}>
+                    <div className={styles['email-wrap']}>
+                        <div className={styles['text']}>이메일</div>
+                        <InputBox
+                            className={'input-bar'}
+                            type={'text'}
+                            value={email}
+                            placeholder={'이메일을 입력해주세요.'}
+                            onChange={onChangeEmail}
+                            onKeyDown={(e) => {
+                                if (e.key === 'Enter') subjectRef.current.focus();
+                            }}
+                        />
+                    </div>
+                    <div className={styles['subject-wrap']}>
+                        <div className={styles['text']}>제목</div>
+                        <InputBox
+                            className={'input-bar'}
+                            type={'text'}
+                            value={subject}
+                            placeholder={'제목을 입력해주세요.'}
+                            onChange={onChangeSubject}
+                            reference={subjectRef}
+                            onKeyDown={(e) => {
+                                if (e.key === 'Enter') questionRef.current.focus();
+                            }}
+                        />
+                    </div>
+                    <div className={styles['question-wrap']}>
+                        <textarea
+                            className={styles['input-question']}
+                            type="text"
+                            value={question}
+                            placeholder={'문의 내용을 입력해주세요'}
+                            onChange={onChangeQuestion}
+                        />
+                    </div>
+                    <div className={styles['files-wrap']}>
+                        <div className={styles['text']}>첨부파일</div>
+                    </div>
+                </div>
+            </div>
+            <FixedButton button_name="문의하기" disable={false} onClick={onClickButton} />
+
+        </>
     );
 };
 
