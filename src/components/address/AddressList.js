303cec7c3a4a7984a90d66894fd8f4e0a364233f
jh <k557330@naver.com>
2020-12-03 13:53:12 +0900

    주석 처리 제거

--- src/components/address/AddressList.js
+++ src/components/address/AddressList.js
@@ -1,51 +1,51 @@
-// import React from 'react';
-// import { ButtonBase} from '@material-ui/core';
-// import styles from './AddressList.module.scss';
+import React from 'react';
+import { ButtonBase} from '@material-ui/core';
+import styles from './AddressList.module.scss';
 
-// import address_icon from '../../static/asset/svg/main/address.svg';
+import address_icon from '../../static/asset/svg/main/address.svg';
 
-// /* 주소 아이템
-//     jibunAddr : 지번
-//     roadAddr : 도로명 혹은 상세주소
-//     distance : 현재위치와의 거리
-//     onClick : 클릭 이벤트
-// */
-// const AddressItem = ({ jibunAddr, roadAddr, distance, onClick }) => {
-//     return (
-//         <ButtonBase className={styles['address-item']} onClick={onClick}>
-//             <div className={styles['icon']}>
-//                 <img src={address_icon} alt={jibunAddr} />
-//             </div>
-//             <div className={styles['pd-box']}>
-//                 <div className={styles['item-info']}>
-//                     <div className={styles['jibun-addr']}>{jibunAddr}</div>
-//                     <div className={styles['road-addr']}>
-//                         {roadAddr}
-//                     </div>
-//                 </div>
-//             </div>
-//             {distance &&        <div className={styles['distance']}>12.24km</div>}
+/* 주소 아이템
+    jibunAddr : 지번
+    roadAddr : 도로명 혹은 상세주소
+    distance : 현재위치와의 거리
+    onClick : 클릭 이벤트
+*/
+const AddressItem = ({ jibunAddr, roadAddr, distance, onClick }) => {
+    return (
+        <ButtonBase className={styles['address-item']} onClick={onClick}>
+            <div className={styles['icon']}>
+                <img src={address_icon} alt={jibunAddr} />
+            </div>
+            <div className={styles['pd-box']}>
+                <div className={styles['item-info']}>
+                    <div className={styles['jibun-addr']}>{jibunAddr}</div>
+                    <div className={styles['road-addr']}>
+                        {roadAddr}
+                    </div>
+                </div>
+            </div>
+            {distance &&        <div className={styles['distance']}>12.24km</div>}
      
-//         </ButtonBase>
-//     );
-// };
+        </ButtonBase>
+    );
+};
 
-// export default ({addr_list,onClick})=>{
-//     const list = addr_list.map((addr,index)=>(
-//         <AddressItem
-//             key={index}
-//             index={index}
-//             jibunAddr={addr.jibunAddr}
-//             roadAddr={addr.roadAddr}
-//             post_num={addr.zipNo}
-//             distance={addr.distance}
-//             onClick={() => onClick(addr.jibunAddr)}
-//         />
-//     ))
-//     return(
-//         <>
-//         {list}
-//         </>
-//     )
-// }
+export default ({addr_list,onClick})=>{
+    const list = addr_list.map((addr,index)=>(
+        <AddressItem
+            key={index}
+            index={index}
+            jibunAddr={addr.jibunAddr}
+            roadAddr={addr.roadAddr}
+            post_num={addr.zipNo}
+            distance={addr.distance}
+            onClick={() => onClick(addr.jibunAddr)}
+        />
+    ))
+    return(
+        <>
+        {list}
+        </>
+    )
+}
 
