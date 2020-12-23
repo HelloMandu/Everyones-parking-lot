22a104c40ceccb0fa36087f33e9fb35c47f389f6
ckdwns9121 <ckdwns9121@naver.com>
2020-12-23 14:53:00 +0900

    # 로드뷰 panoId  수정

--- src/components/modal/RoadviewModal.js
+++ src/components/modal/RoadviewModal.js
@@ -16,8 +16,11 @@ const cx = cn.bind(styles);
 
 const RoadviewModal = (props) => {
 
+    const {lat, lng} =props;
     useEffect(() => {
-        roadView();
+        if(lat && lng){
+            roadView();
+        }
     // eslint-disable-next-line react-hooks/exhaustive-deps
     }, [props]);
 
@@ -26,7 +29,7 @@ const RoadviewModal = (props) => {
         var roadview = new kakao.maps.Roadview(roadviewContainer); //로드뷰 객체
         var roadviewClient = new kakao.maps.RoadviewClient(); //좌표로부터 로드뷰 파노ID를 가져올 로드뷰 helper객체
 
-        var position = new kakao.maps.LatLng(props.lat, props.lng);
+        var position = new kakao.maps.LatLng(lat,lng);
 
         // 특정 위치의 좌표와 가까운 로드뷰의 panoId를 추출하여 로드뷰를 띄운다.
         roadviewClient.getNearestPanoId(position, 50, function (panoId) {
